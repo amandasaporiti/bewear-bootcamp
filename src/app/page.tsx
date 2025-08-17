d6eff61f0dcc01158c63ff2ced6644a8ee36fa25
amandasaporiti <amandasaporiti@gmail.com>
2025-08-17 20:02:08 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+import { Brands } from "@/components/common/brands"
+import { Footer } from "@/components/common/footer"
 import { Header } from "@/components/common/header"
 import { db } from "@/db"
+import { productTable } from "@/db/schema"
+import { desc } from "drizzle-orm"
 import Image from "next/image"
-import { ProductList } from "./auth/components/product-list"
+import { CategorySelector } from "../components/common/category-selector"
+import { ProductList } from "../components/common/product-list"
 
 export default async function Home() {
   const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  })
+
+  const categories = await db.query.categoryTable.findMany({})
+
+  const newlyProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
     with: {
       variants: true,
     },
@@ -25,8 +39,18 @@ export default async function Home() {
           />
         </div>
 
+        {/* Marcas parceiras */}
+        <div className="px-5">
+          <Brands />
+        </div>
+
         {/* Lista de Produtos mais vendidos */}
         <ProductList products={products} title="Mais vendidos" />
+
+        {/* Categorias */}
+        <div className="px-5">
+          <CategorySelector categories={categories} />
+        </div>
 
         {/* Banner secundário */}
         <div className="px-5">
@@ -39,6 +63,11 @@ export default async function Home() {
             className="h-auto w-full"
           />
         </div>
+
+        {/* Novos produtos */}
+        <ProductList title="Novos produtos" products={newlyProducts} />
+
+        <Footer />
       </div>
     </>
   )
