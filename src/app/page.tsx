d0200cf126906142ca9a4f4336be47dc45a1b8ce
amandasaporiti <amandasaporiti@gmail.com>
2025-08-16 21:15:58 -0300

    feat: add header and sign out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import { Button } from "@/components/ui/button"
+import { Header } from "@/components/common/header"
 
 export default function Home() {
-  return <Button>Home</Button>
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
